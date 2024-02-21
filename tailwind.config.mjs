525efe3f748f99f0596b784c1229f03df75fac37
KJohnson82 <rojohnson@gmail.com>
2024-02-21 17:02:52 -0600

    add daisy

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './public/fonts/*',],
 	theme: {
-		extend: {},
+		extend: {
+			
+		},
+		fontFamily: {
+			sans: ['Oxygen', 'sans-serif'],
+			serif: ['DMSerifDisplay','serif'],
+		},
 	},
-	plugins: [],
+	plugins: [require('@tailwindcss/typography'), require("daisyui")],
+	daisyui: {
+		themes: ["business", "dracula", "fantasy"]
+	}
 }
