fc93ab76706ef07747d0cb7b80a457d5106b5ec3
KJohnson82 <rojohnson@gmail.com>
2024-04-03 17:10:07 -0500

    Launch position

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+
 fn main() {
   tauri::Builder::default()
+    .plugin(tauri_plugin_positioner::init())
+    .on_system_tray_event(|app, event| {
+      tauri_plugin_positioner::on_tray_event(app, &event);
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
