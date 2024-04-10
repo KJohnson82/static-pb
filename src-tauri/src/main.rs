6de3f44241c9fcf3095990590d1371beda47cc9c
KJohnson82 <rojohnson@gmail.com>
2024-04-10 17:13:45 -0500

    rounding corners sucks

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,25 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
 
+use tauri::Manager;
+use window_vibrancy::apply_blur;
 
 fn main() {
   tauri::Builder::default()
     .plugin(tauri_plugin_positioner::init())
+    .setup(|app| {
+      let window = app.get_window("main").unwrap();
+
+      #[cfg(target_os = "macos")]
+      apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, Some(16.0))
+        .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+      #[cfg(target_os = "windows")]
+      apply_blur(&window, Some((18, 18, 18, 125)))
+        .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+
+      Ok(())
+    })
     .on_system_tray_event(|app, event| {
       tauri_plugin_positioner::on_tray_event(app, &event);
     })
