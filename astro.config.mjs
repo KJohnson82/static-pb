124e9bdcc69aef1024749ae2c92dfd4d0d8ef9bc
KJohnson82 <rojohnson@gmail.com>
2024-04-29 16:14:54 -0500

    Cleaned up Files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import pagefind from "astro-pagefind";
 import icon from "astro-icon";
-
-import compress from "astro-compress";
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,5 +15,5 @@ export default defineConfig({
   },
   integrations: [tailwind(), icon({
     iconDir: "src/icons"
-  }), pagefind(), compress()]
+  }), pagefind(), playformCompress()]
 });
