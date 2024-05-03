23feb8e173aedf2c1aca64ee87b257f12676ed7e
KJohnson82 <rojohnson@gmail.com>
2024-05-03 17:07:51 -0500

    updates

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,7 +13,6 @@ export default {
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     themes: [
-      "business",
       {
         mcapp: {
           primary: "#1C658C",
